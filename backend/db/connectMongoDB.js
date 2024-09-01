c607d189eb34e68ba0ce23982d164589ae015866
rajZadafiya55 <zadagfiyaraj395.com>
2024-09-01 09:54:55 +0530

    Merge branch 'main' of https://github.com/rajZadafiya55/twitter-clone-Main

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-<<<<<<< HEAD
 const connectMongoDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
@@ -11,20 +10,4 @@ const connectMongoDB = async () => {
   }
 };
 
-=======
-const connectMongoDB = async() =>{
-    try 
-    {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`mongoDb connected: ${conn.connection.host}`);
-    }
-    catch(error)
-    {
-        console.error(`Error connection to mongoDB: ${error.message}`);
-        process.exit(1);
-
-    }
-} 
-
->>>>>>> 3a13693152e846d21da9435cad54d949d37ac1c5
 export default connectMongoDB;
