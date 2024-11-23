1e326195d425db1e0cdd49e7fce04fc56a4574bd
rajZadafiya55 <zadagfiyaraj395.com>
2024-11-23 20:58:28 +0530

    vercel app chnage

--- backend/server.js
+++ backend/server.js
@@ -45,13 +45,24 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
+
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+  app.use(express.static(frontendPath));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(frontendPath, "index.html"));
   });
 }
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to twitter");
