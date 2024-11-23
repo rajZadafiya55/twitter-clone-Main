338445294b73668744c703d4ebe45509e85f1bc8
rajZadafiya55 <zadagfiyaraj395.com>
2024-11-23 20:39:05 +0530

    vercel app chnage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{ 
+  server: {
     port: 3000,
-    proxy:{
-      "/api":{
-        target: "https://twitter-api-st2l.onrender.com",
-        changeOrigin:true,
+    proxy: {
+      "/api": {
+        // target: "https://twitter-api-st2l.onrender.com", 23/11 ye comment karu che
+        target: "https://twitter-clone-main-uldb.onrender.com",
+        // target: "http://localhost:7000/",
+        changeOrigin: true,
       },
-    },  
+    },
   },
 });
